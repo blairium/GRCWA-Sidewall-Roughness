41c8b2ed4527537145c3c82f6a7ce382f511ed83
Blair Haydon <71422563+blairium@users.noreply.github.com>
2025-12-02 11:19:33 +1100

    Init Commit

--- SRC/grcwa-sidewall-roughness/__init__.py
+++ SRC/grcwa-sidewall-roughness/__init__.py
@@ -1,1 +1,5 @@
-"""GRCWA-Sidewall-Roughness."""
+from data import get_optical_constants
+from RCWA import calculate_first_order_transmission
+from Roughness import sidewall_roughness, apply_roughness
+from plotting import plot_grating_schematic
+from run import run
